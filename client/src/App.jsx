5442f66b6fa6763ce29f1ced3c03de7b92119077
mrbillionn <kyaligonzawarren44@gmail.con>
2023-06-27 13:04:19 +0300

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import ChatBody from './components/ChatBody';
+import ChatInput from './components/ChatInput';
 
 function App() {
 
   return (
-   <div className='bg-[#1A232E] h-screen py-6 relative sm:px-28 text-white overflow-hidden flex flex-col justify-between align-middle '>
+   <div className='bg-[#1A232E] h-screen py-6 relative sm:px-28 px-16 
+   text-white overflow-hidden flex flex-col justify-between align-middle '>
 
 {/* gradients */}
 <div className='gradient-01 z-0 absolute'></div>
@@ -19,7 +21,9 @@ function App() {
     </div>
 
     {/* input */}
-    <div>Input</div>
+    <div className='w-full max-w-4xl min-w-[20rem] self-center'>
+      <ChatInput/>
+    </div>
    </div>
   )
 }
