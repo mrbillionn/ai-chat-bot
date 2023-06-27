a0312aecb3a3aa2e89518190728e75a6bd69635c
mrbillionn <kyaligonzawarren44@gmail.con>
2023-06-27 15:43:55 +0300

    test

--- client/src/components/ChatBody.jsx
+++ client/src/components/ChatBody.jsx
@@ -1,30 +1,45 @@
-import React from 'react'
+import React, { useRef } from "react";
+import autoAnimate from "@formkit/auto-animate";
+import { useEffect } from "react";
 
-const ChatBody = () => {
 
-    const aiStyle = "bg-white bg-opacity-40 backdrop-blur-lg dropshadow-md mr-auto"
+const ChatBody = ({ chat }) => {
+  const aiStyle =
+    "bg-white bg-opacity-40 backdrop-blur-lg dropshadow-md mr-auto";
+
+    const parent = useRef(null);
+    const bottomRef = useRef(null);
+
+    // only for aut animations
+    useEffect(()=>{
+        parent.current && autoAnimate(parent.current);
+    }, [parent])
+
+    //for scrolling bottom
+    useEffect(()=>{
+        bottomRef.current?.scrollIntoView({behavior: "smooth"})
+    }, [chat])
 
   return (
-    <div className="flex flex-col gap-4">
-
-        {/* client message */}
-        <div className='border-[#999999] break-word border-2 rounded-xl self-end px-3 py-3 max-w-[80%]'>
-            <pre className='whitespaces-pre-wrap'>
-                <span>Hi Chat GPT , Can You help me</span>
+    <div className="flex flex-col gap-4" ref={parent}>
+      {chat.map((message, i) => {
+        return (
+          <div
+            key={i}
+            className={`border-[#999999] break-words border-2 rounded-xl self-end px-3 py-3 max-w-[80%] ${
+              message.sender === "ai" && aiStyle
+            }`}
+          >
+            <pre className="whitespace-pre-wrap">
+              <span>{message.message}</span>
             </pre>
-        </div>
+          </div>
+        );
+      })}
 
-{/* ai response */}
-<div className={`border-[#999999] break-word border-2 rounded-xl self-end px-3 py-3 max-w-[80%] '>
-    <pre className='whitespaces-pre-wrap ${aiStyle}`}>
-    <pre>
-        <span>
-            Yeah, I can Help You with anything
-        </span>
-    </pre>
-</div>
+      <div ref={bottomRef} className="h-3"></div>
     </div>
-  )
-}
+  );
+};
 
-export default ChatBody
+export default ChatBody;
