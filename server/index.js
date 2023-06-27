925de6dfd6ea39319173aec5725423fe1251bc20
mrbillionn <kyaligonzawarren44@gmail.con>
2023-06-27 16:03:50 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -34,12 +34,12 @@ app.post('/', async (req, res) => {
     try {
          
         const response = await openai.complete({
-        model:"text-devinci-003",
+        model:"text-davinci-003",
         prompt: `${message}`,
         max_tokens:100,
         temperature: .5,
         })
-    res.json({message: response.data.choices[0].value})
+    res.json({message: response.data.choices[0].text})
     
     } catch(e){
         console.log(e)
