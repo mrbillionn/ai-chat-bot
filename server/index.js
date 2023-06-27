2964f629c4af5673aaa5e05ce7e1aa28c9732ff1
mrbillionn <kyaligonzawarren44@gmail.con>
2023-06-27 14:11:04 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,3 +3,17 @@ import cors from 'cors'
 import bodyParser from 'body-parser'
 import env from 'dotenv'
 import {Configuration, OpenAIApi} from 'openai-api'
+
+const app = express()
+
+
+env.config()
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.listen("3080", () =>console.log("Server is running on port 3080"))
+
+app.get("/", (req, res) => {
+    res.send("Hello World")
+})
