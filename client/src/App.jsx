6475fa1f8b98c7d4b77fe0aa2aa87f08bb0a9dc5
mrbillionn <kyaligonzawarren44@gmail.con>
2023-06-27 13:37:15 +0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,32 +2,30 @@ import ChatBody from './components/ChatBody';
 import ChatInput from './components/ChatInput';
 
 function App() {
-
   return (
-   <div className='bg-[#1A232E] h-screen py-6 relative sm:px-16 px-12
-   text-white overflow-hidden flex flex-col justify-between align-middle '>
+    <div className='bg-[#1A232E] h-screen py-6 relative sm:px-16 px-12 text-white overflow-hidden flex flex-col justify-between align-middle'>
 
-{/* gradients */}
-<div className='gradient-01 z-0 absolute'></div>
-<div className='gradient-02 z-0 absolute'></div>
+      {/* gradients */}
+      <div className='gradient-01 z-0 absolute'></div>
+      <div className='gradient-02 z-0 absolute'></div>
 
-    {/* header */}
-    <div className='uppercase font-bold text-2xl text-center
-     mb-3'>Warren ai</div>
+      {/* header */}
+      <div className='uppercase font-bold text-2xl text-center mb-3'>Warren ai</div>
 
-    {/* body */}
-    <div className='h-[90%] overflow-auto w-full max-w-4xl min-w-[20rem] py-8 px-4 self-center '
-            style={{ overscrollBehavior: 'none' }} // Disable pull-to-refresh behavior
-            >
-      <ChatBody/>
+      {/* body */}
+      <div
+        className='h-[90%] overflow-y-auto w-full max-w-4xl min-w-[20rem] py-8 px-4 self-center'
+        style={{ overscrollBehavior: 'none' }} // Disable pull-to-refresh behavior
+      >
+        <ChatBody />
+      </div>
+
+      {/* input */}
+      <div className='w-full max-w-4xl min-w-[20rem] self-center'>
+        <ChatInput />
+      </div>
     </div>
-
-    {/* input */}
-    <div className='w-full max-w-4xl min-w-[20rem] self-center'>
-      <ChatInput/>
-    </div>
-   </div>
-  )
+  );
 }
 
-export default App
+export default App;
