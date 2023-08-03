58447b011684e89ffe371ab9da3950b02da7d300
mrbillionn <kyaligonzawarren44@gmail.con>
2023-08-03 21:15:43 +0300

    ai name

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -30,7 +30,7 @@ function App() {
 
       {/* header */}
       <div className="uppercase font-bold  text-2xl text-center mb-3">
-        ChatGpt2.0
+        Warren AI
       </div>
 
       {/* body */}
--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 export const fetchResponse =  async(chat) => {
     try {
         // after depoloyment you should change the fetch URL below
-        const response = await fetch('https://ai-chat-bot-two.vercel.app/', { 
+        const response = await fetch('https://server-ai-bot.vercel.app/', { 
             method: 'POST',
             headers: {
                 "Content-Type": "application/json"
